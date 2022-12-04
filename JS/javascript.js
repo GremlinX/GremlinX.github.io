0f4aa1db199f140602ae546ad4d78064c862f5a3
Gremlin <50713742+GremlinX@users.noreply.github.com>
2022-12-04 17:35:28 -0300

    Ajusta layout e design

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,4 +1,11 @@
 function useDarkMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
+
+  var moon = document.getElementById("moon").innerHTML;
+  if (moon === '<i class="fas fa-moon" aria-hidden="true"></i>') {
+    document.getElementById("moon").innerHTML = '<i class="fas fa-sun"></i>';
+  } else {
+    document.getElementById("moon").innerHTML = '<i class="fas fa-moon"></i>';
+  }
 }
